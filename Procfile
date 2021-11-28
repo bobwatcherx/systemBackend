05d9cc01401c9c15f38e14348dde69f4ae46fd34
root <bobwatcherx@gmail.com>
2021-11-27 21:33:35 -0500

    deno heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: deno run --allow-net=:${PORT} --cached-only main.js --port=${PORT}
--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import { DB } from "https://deno.land/x/sqlite/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 // let location = Deno.openSync("datamain.sqlite",{read:true, write:true})
 import { v4 } from "https://deno.land/std@0.91.0/uuid/mod.ts";
-import { hash, verify } from "https://deno.land/x/scrypt/mod.ts";
+import { createHash } from "https://deno.land/std@0.91.0/hash/mod.ts";
+import * as flags from 'https://deno.land/std/flags/mod.ts';
 
 const file = Deno.openSync("datamain.sqlite", { read: true, write: true });
 const db = new DB("datamain.sqlite");
@@ -22,11 +23,20 @@ const body = ctx.request.body({ type: 'form' })
   const value = await body.value
   let username = value.get('username')
   let password = value.get('password')
+      let hashp = createHash("md5");
+hashp.update(password);
+let haspresult = hashp.toString();
   try{
-    let query = db.query("select * from tbluser where username = ? and password = ?",[username,password])
+    let query = db.query("select * from tbluser where username = ? and password = ? ",[username,haspresult])
     if(query.length ==  1){
-      console.log(query.indexOf(3))
-      ctx.response.body = "user found"
+      let sip = Object.values(query[0])
+      console.log(sip[2])
+      ctx.response.body = {"find":"user found","data":{
+        "username": sip[1],
+        "password":sip[3],
+        "nik":sip[2],
+        "active":sip[4]
+      }}
     }
   }catch(err){
     ctx.response.body = {"error":"not found"}
@@ -44,7 +54,9 @@ const body = ctx.request.body({ type: 'form' })
   let nik = value.get('nik')
 // v4 unique id
 let uniqid = v4.generate();
-const hashResult = await hash(password || "");
+let hash = createHash("md5");
+hash.update("dush");
+let hashResult = hash.toString();
   try{
     let query = db.query(`insert into tbluser
      (id,username,password,nik,active) values ('${uniqid}','${username}','${hashResult}','${nik}','not')`)
@@ -59,13 +71,24 @@ const hashResult = await hash(password || "");
 //SKRIPSI
 router.get("/",(ctx)=>{
   let query = db.query("select * from tblsekolah");
-  ctx.response.body = query
+  let temp = []
+  let main = Object.values(query).forEach((key)=>{
+
+   temp.push({"nama":key[1],"judul":key[3],"jurusan":key[2],"date":key[3],"status":key[4]})
+   console.log(temp)
+   ctx.response.body = {"status":"succes","data":temp}
+  })
+
 })
 router.get("/details/:nama",(ctx)=>{
   let nama = ctx.params.nama
   let query = db.query("select * from tblsekolah where nama = ?",[nama])
-  console.log(query)
-  ctx.response.body = query
+  let temp = []
+  let main = Object.values(query).forEach((key)=>{
+
+  temp.push({"id":key[0],"nama":key[1],"jurusan":key[2],"judul":key[3],"date":key[4],"status":key[5]})
+  })
+  ctx.response.body = temp
 })
 router.post("/add",async(ctx)=>{
    const body = ctx.request.body({ type: 'form' })
@@ -118,8 +141,13 @@ router.put("/verified/:id",(ctx)=>{
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.addEventListener(
-  "listen",
-  (e) => console.log("Listening on http://localhost:8080"),
-);
-await app.listen({ port: 8080 });
+const {args, exit} = Deno;
+const DEFAULT_PORT = 8000;
+const argPort = flags.parse(args).port;
+const port = argPort ? Number(argPort) : DEFAULT_PORT;
+if (isNaN(port)){
+console.log("This is not port number");
+exit(1);
+};
+console.log("server runn on" +  DEFAULT_PORT)
+await app.listen({ port: port });
